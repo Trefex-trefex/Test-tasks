a99ff3ba0b3477c69b1ff142796db0716e1e5bb3
Trefex-trefex <misha.friday@mail.ru>
2025-03-03 19:41:59 +0300

    Created project and required dependencies

---
+++ WeatherArchive/Models/WeatherRecord.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WeatherArchive.Models
+{
+    public class WeatherRecord
+    {
+        public int Id { get; set; } 
+        public DateTime Date { get; set; }
+        public float Temperature { get; set; }
+    }
+}
